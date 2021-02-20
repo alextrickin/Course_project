332b6b9c4f6529cec6346f7365a42baf0a575379
alextrickin <alextrickin@gmail.com>
2021-02-20 14:23:19 +0400

    Initial for gallerey

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
-  var mySwiper = new Swiper('.main-top-swiper', {
+  var main_top_Swiper = new Swiper('.main-top-swiper', {
     // Optional parameters    
     loop: true,
 
@@ -14,5 +14,26 @@ window.addEventListener('DOMContentLoaded', function () {
       nextEl: '.main-top-button-prev',
       prevEl: '.main-top-button-next',
     },
+  });
+  var gallery_Swiper = new Swiper('.gallery-swiper', {
+    // Optional parameters    
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.gallery-pagination',
+      clickable: 'true'
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.gallery-button-prev',
+      prevEl: '.gallery-button-next',
+    },
+    slidesPerView: 3,
+    slidesPerColumn: 2,
+    slidesPerColumnFill: "column",
+    slidesPerGroup: 3,
+    spaceBetween: 34
   });
 })
