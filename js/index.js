a83adfd857b6f2f407af818659348686e09ad8a9
Alex Trickin <alextrickin@mail.ru>
2021-02-24 11:07:26 +0400

    Added swiper to gallery for 320

--- js/index.js
+++ js/index.js
@@ -1,39 +1,40 @@
-window.addEventListener('DOMContentLoaded', function () {
-  var main_top_Swiper = new Swiper('.main-top-swiper', {
-    // Optional parameters    
-    loop: true,
+window.addEventListener('DOMContentLoaded', function() {
+    var main_top_Swiper = new Swiper('.main-top-swiper', {
+        // Optional parameters    
+        loop: true,
 
-    // If we need pagination
-    pagination: {
-      el: '.main-top-pagination',
-      clickable: 'true'
-    },
+        // If we need pagination
+        pagination: {
+            el: '.main-top-pagination',
+            clickable: 'true'
+        },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.main-top-button-prev',
-      prevEl: '.main-top-button-next',
-    },
-  });
-  var gallery_Swiper = new Swiper('.gallery-swiper', {
-    // Optional parameters    
-    loop: true,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.main-top-button-prev',
+            prevEl: '.main-top-button-next',
+        },
+    });
+    var gallery_Swiper = new Swiper('.gallery-swiper', {
+        // Optional parameters    
+        loop: true,
 
-    // If we need pagination
-    pagination: {
-      el: '.gallery-pagination',
-      clickable: 'true'
-    },
+        // If we need pagination
+        pagination: {
+            el: '.gallery-pagination',
+            clickable: 'true'
+        },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.gallery-button-prev',
-      prevEl: '.gallery-button-next',
-    },
-    slidesPerView: 3,
-    slidesPerColumn: 2,
-    slidesPerColumnFill: "column",
-    slidesPerGroup: 3,
-    spaceBetween: 34
-  });
+        // Navigation arrows
+        navigation: {
+            nextEl: '.gallery-button-prev',
+            prevEl: '.gallery-button-next',
+        },
+        initialSlide: 12,
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        slidesPerColumnFill: "column",
+        slidesPerGroup: 1,
+        spaceBetween: 34
+    });
 })
